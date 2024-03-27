87d0d97e8918e81ce012e167b5c2716dc02baf47
Gabriel Ignacio Urushima <74004982+giurushima@users.noreply.github.com>
2024-03-27 13:55:11 -0300

    Update scriptuserdocker.sh

--- scripts-bash-vm/scriptuserdocker.sh
+++ scripts-bash-vm/scriptuserdocker.sh
@@ -5,8 +5,6 @@
 # Creacion de usuario "userdocker" con permisos de Docker.
 sudo adduser userdocker
 sudo usermod -aG docker userdocker
-sudo chown -R userdocker:docker /home/userdocker
-sudo chmod -R 775 /home/userdocker
 sudo systemctl status docker
 sudo systemctl enable docker
 sudo systemctl start docker
